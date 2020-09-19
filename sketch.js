c7bd27dc68f766cca84020a5b78f243ec837106d
anwesha-cloud <anweshamaitin2005@gmail.com>
2020-09-19 12:53:42 +0530

    adding score

--- sketch.js
+++ sketch.js
@@ -5,6 +5,14 @@ var mask,mask_img
 var sanitizer,sanitizer_img
 var booster,booster_img
 var bg,bg_img
+var ground
+var germ1Group
+var germ2Group
+var maskGroup
+var sanitizerGroup
+var boosterGroup
+var count=0
+var healthmeter=200
 
 function preload(){
   max_img = loadAnimation("images/max.png","images/max2.png","images/max3.png")
@@ -19,45 +27,115 @@ function setup() {
   createCanvas(1050,500);
 max = createSprite(80, 440, 50, 50);
 max.addAnimation("img", max_img)
-max.scale=0.5
-mask= createSprite(600, 200, 50, 50);
-mask.addImage("msk", mask_img)
-mask.scale=0.2
-sanitizer= createSprite(100, 300, 50, 50);
-sanitizer.addImage("snt", sanitizer_img)
-sanitizer.scale=0.2
-booster= createSprite(700, 200, 50, 50);
-booster.addImage("bst", booster_img)
-booster.scale=0.5
+max.scale=0.8
+
+ground=createSprite(525,490,1050,20);
+ground.shapeColor="black"
+germ1Group= new Group()
+germ2Group= new Group()
+maskGroup= new Group()
+sanitizerGroup= new Group()
+boosterGroup=new Group()
+
 }
 
 function draw() {
-  background(bg_img);  
-
+  background(bg_img); 
+  
   if(keyDown("RIGHT_ARROW")){
     max.velocityX=3;
   }
   if(keyWentUp("RIGHT_ARROW")){
     max.velocityX=0;
   }
-  max.depth=mask.depth
-  max.depth=max.depth+1
+  if(keyDown("UP_ARROW")){
+    max.velocityY=-2;
+  }
+  if(keyWentUp("UP_ARROW")){
+    max.velocityY=2;
+  }
+  if(max.isTouching(maskGroup)){
+    count=count+1
+  }
+  if(max.isTouching(sanitizerGroup)){
+    count=count+1
+  }
+  if(max.isTouching(boosterGroup)){
+    count=count+2
+  }
+   
+  max.collide(ground)
 
-  spawnGerms()
+  spawnGerm1()
+  spawnGerm2()
+  spawnMask()
+  spawnSanitizer()
+  spawnBooster()
+
+  if(max.x===1050){
+    max.x=80
+  }
+  if(max.isTouching(germ1Group)){
+    max.x=80
+    healthmeter=healthmeter-1
+  }
+  if(max.isTouching(germ2Group)){
+    max.x=80
+    healthmeter=healthmeter-1
+  }
+
   drawSprites();
+  fill("white") 
+  textSize(20)
+  text("SCORE:"+count,900,30)
+  fill("white") 
+  textSize(20)
+  text("HEALTH METER:"+healthmeter,30,30)
+  
 }
 
-function spawnGerms(){
-  if(World.frameCount%100===0){
-    var germ= createSprite(1050,Math.round(random(330,470)), 50, 50);
+function spawnGerm1(){
+  if(World.frameCount%380===0){
+    var germ= createSprite(1000,Math.round(random(80,470)), 50, 50);
         germ.addImage("g1", germ_img)
         germ.scale=0.1
-        germ.velocityX=-1
+        germ.velocityX=-6
+        germ1Group.add(germ)
   }
+}
+function spawnGerm2(){
   if(World.frameCount%300===0){
-    var germ2= createSprite(1050,Math.round(random(300,490)), 50, 50);
+    var germ2= createSprite(1000,Math.round(random(100,490)), 50, 50);
         germ2.addImage("g2", germ2_img)
         germ2.scale=0.2
-        germ2.velocityX=-1
+        germ2.velocityX=-4
+        germ2Group.add(germ2)
+  }
+}
+function spawnMask(){
+  if(World.frameCount%430===0){
+   var mask= createSprite(1000,Math.round(random(50,450)), 50, 50);
+       mask.addImage("msk", mask_img)
+       mask.scale=0.2
+       mask.velocityX=-1
+      maskGroup.add(mask)
+  }
+}
+function spawnSanitizer(){
+  if(World.frameCount%450===0){
+   var sanitizer= createSprite(1000,Math.round(random(50,450)), 50, 50);
+       sanitizer.addImage("snt", sanitizer_img)
+       sanitizer.scale=0.2
+       sanitizer.velocityX=-4
+      sanitizerGroup.add(sanitizer)
+  }
+}
+function spawnBooster(){
+  if(World.frameCount%550===0){
+   var booster= createSprite(1000,Math.round(random(150,460)), 50, 50);
+       booster.addImage("bst", booster_img)
+       booster.scale=0.5
+       booster.velocityX=-8
+       boosterGroup.add(booster)
   }
 }
